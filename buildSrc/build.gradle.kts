c5951e0a8f7f7077d0662a33f50092fa4e83bf98
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-11-01 17:52:16 +0100

    Downgrade android gradle plugin
    Due to 4.2.0-alpha15 being buggy
    with gradle kotlin DSL

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.2.0-alpha15")
+    implementation("com.android.tools.build:gradle:4.2.0-alpha13")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
 
     // NOTE: Do not place your application dependencies here; they belong
