caebd95874eb4e70962bebfd5c49f3a345038d00
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-11-30 17:49:59 +0100

    Update compose version

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -10,7 +10,7 @@ object Versions {
 
     const val rainbowcake = "1.2.0"
 
-    const val compose = "1.0.0-alpha06"
+    const val compose = "1.0.0-alpha07"
     const val material = "1.2.1"
     const val pagingCompose = "1.0.0-alpha02"
 
