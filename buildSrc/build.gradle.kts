140a88c375423d286bb0163f43c9fe9aae5bc94f
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-12-02 17:45:55 +0100

    Update gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.2.0-beta01")
+    implementation("com.android.tools.build:gradle:7.0.0-alpha01")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10")
     implementation("com.google.dagger:hilt-android-gradle-plugin:2.29-alpha")
 
