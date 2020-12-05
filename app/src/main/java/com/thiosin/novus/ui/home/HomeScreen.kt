c2530e3f15f19ebb9b5318ca8e874dfd412ba082
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-12-05 22:07:53 +0100

    Add drawer icon to topbar

--- app/src/main/java/com/thiosin/novus/ui/home/HomeScreen.kt
+++ app/src/main/java/com/thiosin/novus/ui/home/HomeScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.loadVectorResource
 import androidx.paging.PagingData
+import com.thiosin.novus.R
 import com.thiosin.novus.domain.model.SubmissionPreview
 import com.thiosin.novus.ui.common.SubmissionList
 import kotlinx.coroutines.flow.Flow
@@ -18,7 +20,16 @@ fun HomeContent(
     Scaffold(
         modifier = Modifier.fillMaxWidth(),
         topBar = {
-            TopAppBar(title = { Text(text = "/r/$subreddit") })
+            TopAppBar(
+                title = { Text(text = "/r/$subreddit") },
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                        loadVectorResource(id = R.drawable.ic_baseline_menu_24).resource.resource?.let {
+                            Icon(asset = it)
+                        }
+                    }
+                }
+            )
         },
         bodyContent = {
             Surface(color = MaterialTheme.colors.background) {
