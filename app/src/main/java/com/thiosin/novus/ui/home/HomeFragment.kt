c6f6d9a0c1219e498beb50e8d016b5192e49e3a2
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-12-06 14:14:16 +0100

    Add custom top app bar

--- app/src/main/java/com/thiosin/novus/ui/home/HomeFragment.kt
+++ app/src/main/java/com/thiosin/novus/ui/home/HomeFragment.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import co.zsmb.rainbowcake.base.OneShotEvent
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.navigation.navigator
 import com.thiosin.novus.di.getViewModel
+import com.thiosin.novus.ui.common.NovusTopAppBar
+import com.thiosin.novus.ui.common.SubmissionList
 import com.thiosin.novus.ui.home.HomeViewModel.ShowLinkEvent
 import com.thiosin.novus.ui.theme.NovusTheme
 import com.thiosin.novus.ui.web.WebFragment
@@ -22,7 +32,7 @@ class HomeFragment : RainbowCakeFragment<HomeViewState, HomeViewModel>() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
@@ -39,17 +49,40 @@ class HomeFragment : RainbowCakeFragment<HomeViewState, HomeViewModel>() {
     override fun render(viewState: HomeViewState) {
         (view as ComposeView).setContent {
             NovusTheme {
-                when (viewState) {
-                    is HomeInitial -> Unit
-                    is HomeReady -> HomeContent(
-                        listState = viewState.listState,
-                        subreddit = viewState.subreddit,
-                        onLinkClicked = {
-                            viewModel.showLink(it)
+                Scaffold(
+                    modifier = Modifier.fillMaxWidth(),
+                    topBar = {
+                        NovusTopAppBar(
+                            title = viewState.getTitle()
+                        )
+                    },
+                    bodyContent = {
+                        when (viewState) {
+                            is HomeInitial -> {
+                                Column(modifier = Modifier.fillMaxSize(),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center) {
+                                    CircularProgressIndicator()
+                                }
+                            }
+                            is HomeReady -> {
+                                SubmissionList(
+                                    listFlow = viewState.listState
+                                ) {
+                                    viewModel.showLink(it)
+                                }
+                            }
                         }
-                    )
-                }
+                    }
+                )
             }
+        }
+    }
+
+    private fun HomeViewState.getTitle(): String {
+        return when (this) {
+            HomeInitial -> "Loading"
+            is HomeReady -> "/r/${subreddit}"
         }
     }
 
