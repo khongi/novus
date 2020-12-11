f9d7a3b31e8fae83184b08b7383319bcea07778b
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-12-11 21:26:22 +0100

    Add surface to comments to enable shadows

--- app/src/main/java/com/thiosin/novus/ui/view/SubmissionDetails.kt
+++ app/src/main/java/com/thiosin/novus/ui/view/SubmissionDetails.kt
@@ -1,12 +1,12 @@
 package com.thiosin.novus.ui.view
 
+import android.view.Surface
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -49,22 +49,28 @@ private fun SubmissionContent(
     onVoteClick: (Submission) -> Unit,
 ) {
     val liked = remember { mutableStateOf(submission.likes) }
-    Column(modifier = Modifier.padding(top = 4.dp)) {
-        InfoRow(submission)
-        TitleRow(submission)
-        if (submission.selfText.isBlank().not()) {
-            SelfText(submission.selfText)
-        }
-        submission.media?.let {
-            MediaRow(it, displayWidthDp.dp)
-        }
-        Row(modifier = Modifier.padding(horizontal = 4.dp).fillMaxWidth().height(48.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically) {
-            Votes(submission.votes)
-            UpVoteButton(submission = submission, liked = liked, onClick = onVoteClick)
-            DownVoteButton(submission = submission, liked = liked, onClick = onVoteClick)
-            LinkButton(submission.link, onLinkClick)
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp).fillMaxWidth(),
+        elevation = 8.dp
+    ) {
+        Column(modifier = Modifier.padding(top = 4.dp)) {
+            InfoRow(submission)
+            TitleRow(submission)
+            if (submission.selfText.isBlank().not()) {
+                SelfText(submission.selfText)
+            }
+            submission.media?.let {
+                MediaRow(it, displayWidthDp.dp)
+            }
+            Row(modifier = Modifier.padding(horizontal = 4.dp).fillMaxWidth().height(48.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically) {
+                Votes(submission.votes)
+                UpVoteButton(submission = submission, liked = liked, onClick = onVoteClick)
+                DownVoteButton(submission = submission, liked = liked, onClick = onVoteClick)
+                LinkButton(submission.link, onLinkClick)
+            }
         }
     }
 }
