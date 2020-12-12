e957e5926254fe2752a3074e6e22f440c6a6e55d
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-12-13 00:14:42 +0100

    Move build src dependencies into single file

--- buildSrc/src/main/java/com/thiosin/novus/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/thiosin/novus/buildsrc/dependencies.kt
@@ -1,6 +1,59 @@
 package com.thiosin.novus.buildsrc
 
+object Versions {
+    const val androidGradlePlugin = "7.0.0-alpha01"
+
+    const val compileSdk = 30
+    const val targetSdk = 30
+    const val minSdk = 28
+
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    const val kotlin = "1.4.20"
+
+    const val rainbowcake = "1.3.0"
+
+    const val compose = "1.0.0-alpha08"
+    const val material = "1.2.1"
+
+    const val ktxCore = "1.3.2"
+    const val ktxLifecycle = "2.3.0-beta01"
+    const val appCompat = "1.2.0"
+
+    const val hilt = "2.29-alpha"
+    const val hiltAndroidx = "1.0.0-alpha02"
+    const val hiltGradlePlugin = "2.29.1-alpha"
+
+    const val redditARAW = "v1.0.0"
+    const val redditAuth = "v1.2.2"
+
+    const val accompanist = "0.4.0"
+    const val coil = "1.1.0"
+
+    const val exoplayer = "2.11.8"
+
+    const val okHttp = "4.8.1"
+    const val retrofit = "2.9.0"
+    const val moshi = "1.10.0"
+
+    const val nav = "2.3.2"
+
+    const val krate = "1.0.0"
+
+    object Tests {
+        const val jUnit = "4.13.1"
+        const val jUnitExt = "1.1.2"
+        const val espresso = "3.3.0"
+        const val kotest = "4.3.1"
+        const val mockk = "1.10.2"
+    }
+}
+
 object Dependencies {
+    const val kotlin = "1.4.20"
+    const val compose = "1.0.0-alpha08"
+
     object Kotlin {
         const val stdLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
     }
@@ -98,4 +151,20 @@ object Dependencies {
             const val android = "com.google.dagger:hilt-android-testing:${Versions.hilt}"
         }
     }
+}
+
+object Plugins {
+    const val androidGradle = "com.android.tools.build:gradle:${Versions.androidGradlePlugin}"
+    const val kotlinGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val hiltGradle = "com.google.dagger:hilt-android-gradle-plugin:${Versions.hiltGradlePlugin}"
+    const val safeArgsGradle = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.nav}"
+
+    const val androidApplication = "com.android.application"
+
+    const val kotlinAndroid = "kotlin-android"
+    const val kotlinKapt = "kotlin-kapt"
+    const val kotlinAndroidExtensions = "kotlin-android-extensions"
+
+    const val hilt = "dagger.hilt.android.plugin"
+    const val safeArgs = "androidx.navigation.safeargs.kotlin"
 }
