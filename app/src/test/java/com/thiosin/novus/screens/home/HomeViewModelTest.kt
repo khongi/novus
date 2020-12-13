75e1421a2251ec80b0a3313809da0140f124afd5
Khongor Bayarsaikhan <b.khongi@gmail.com>
2020-12-13 15:30:03 +0100

    Setup nested testing with BehaviorSpec
    [ci skip]

--- app/src/test/java/com/thiosin/novus/screens/home/HomeViewModelTest.kt
+++ app/src/test/java/com/thiosin/novus/screens/home/HomeViewModelTest.kt
@@ -1,11 +1,13 @@
 package com.thiosin.novus.screens.home
 
+import co.zsmb.rainbowcake.test.assertDidObserve
 import co.zsmb.rainbowcake.test.observeStateAndEvents
+import com.thiosin.novus.domain.model.Subreddit
+import com.thiosin.novus.domain.model.SubredditType
 import com.thiosin.novus.domain.model.User
 import com.thiosin.novus.kotest.ViewModelTestListener
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.core.test.TestCase
-import io.kotest.matchers.shouldBe
 import io.mockk.clearAllMocks
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -14,38 +16,56 @@ import io.mockk.mockk
 class HomeViewModelTest : BehaviorSpec({
 
     val homePresenter: HomePresenter = mockk(relaxUnitFun = true)
-    val user: User = mockk()
+    val user = User(name = "username")
+    val defaultSubreddit = Subreddit(
+        queryName = "default queryName",
+        displayName = "default displayName",
+        type = SubredditType.Frontpage,
+    )
+    val nonDefaultSubreddit = Subreddit(
+        queryName = "non default queryName",
+        displayName = "non default displayName",
+        type = SubredditType.Community,
+    )
 
-    Given("Initial state without user") {
-        val vm = HomeViewModel(homePresenter)
-        coEvery { homePresenter.getUser() } returns null
-
-        When ("load without args") {
-            Then ("should acquire userless credentials") {
-                vm.observeStateAndEvents { stateObserver, eventsObserver ->
+    Given("One default, one non default subreddit and no submissions") {
+        coEvery { homePresenter.getDefaultSubreddit() } returns defaultSubreddit
+        coEvery { homePresenter.getSubreddits() } returns listOf(defaultSubreddit, nonDefaultSubreddit)
+        coEvery { homePresenter.getSubredditPage(defaultSubreddit.queryName) } returns listOf()
+        And("Initial state without user") {
+            coEvery { homePresenter.getUser() } returns null
+            When("load without args") {
+                Then("should acquire userless credentials") {
+                    val vm = HomeViewModel(homePresenter)
                     vm.load()
-
                     coVerify { homePresenter.acquireUserlessCredentials() }
+                }
+                Then("default subreddit should be selected") {
+                    val vm = HomeViewModel(homePresenter)
+                    vm.observeStateAndEvents { stateObserver, eventsObserver ->
+                        vm.load()
+
+                        stateObserver.assertDidObserve(HomeReady(
+                            submissions = listOf(),
+                            selectedSubreddit = defaultSubreddit,
+                            subreddits = listOf(defaultSubreddit, nonDefaultSubreddit),
+                            user = null
+                        ))
+                    }
                 }
             }
         }
-    }
-
-    Given("Initial state with user") {
-        val vm = HomeViewModel(homePresenter)
-        coEvery { homePresenter.getUser() } returns user
-
-        When ("load without args") {
-            Then ("should NOT acquire userless credentials") {
-                vm.observeStateAndEvents { stateObserver, eventsObserver ->
-                    vm.load()
-
+        And("Initial state with user") {
+            coEvery { homePresenter.getUser() } returns user
+            When("load without args") {
+                val vm = HomeViewModel(homePresenter)
+                vm.load()
+                Then("should NOT acquire userless credentials") {
                     coVerify(exactly = 0) { homePresenter.acquireUserlessCredentials() }
                 }
             }
         }
     }
-
 }) {
     init {
         listener(ViewModelTestListener())
@@ -53,8 +73,14 @@ class HomeViewModelTest : BehaviorSpec({
 
     override fun beforeTest(testCase: TestCase) {
         super.beforeTest(testCase)
+
+        // Always start from fresh
         if (testCase.description.isRootTest()) {
             clearAllMocks()
+        }
+        // Preserve answers inside containers for nested tests
+        else if (testCase.description.isContainer()) {
+            clearAllMocks(answers = false)
         }
     }
 }
